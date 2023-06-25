a2b0aa131d98fce8e27253606f608bbf7962bb46
moepig <uncho.tnct@gmail.com>
2023-06-25 20:58:20 +0900

    レスポンスを定義

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use serde::{Deserialize, Serialize};
  
+// https://api.slack.com/methods/chat.postMessage#args
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserTokenPostRequest {
     token: String,
     channel: String,
     text: String,
+}
+
+// https://api.slack.com/methods/chat.postMessage#examples
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserTokenPostResponse {
+    ok: bool,
 }
 
 fn main() {

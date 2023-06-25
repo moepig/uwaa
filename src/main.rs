cca4b923670fae59b21ce3b1370b657dfe6bc946
moepig <uncho.tnct@gmail.com>
2023-06-25 22:12:37 +0900

    環境変数読み込み

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+use std::process;
 use serde::{Deserialize, Serialize};
  
 // https://api.slack.com/methods/chat.postMessage#args
@@ -14,6 +16,21 @@ pub struct UserTokenPostResponse {
     ok: bool,
 }
 
+fn must_var(s: &str) -> String {
+    let ret_val  = match env::var(s) {
+        Ok(val) => val,
+        Err(err) => {
+            println!("{}: {}", err, s);
+            process::exit(1);
+        },
+    };
+
+    return ret_val;
+}
+
 fn main() {
+    let token = must_var("UWAA_TOKEN");
+    let channel= must_var("UWAA_CHANNEL");
+
     println!("Hello, world!");
 }
